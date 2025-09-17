53c12b1ac18e6436d905d9c7ef059088d6e4112e
nouzoehiroaki <blowitout2007@yahoo.co.jp>
2025-09-18 00:44:28 +0900

    9/18 footer コピーライト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./reset.css";
 import Header from '../components/Header';
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/app/supportedby/page.tsx
@@ -0,0 +1,12 @@
+import "../../styles/styles.css"
+import './supportedby.css';
+
+export default function SupportedBy() {
+  return (
+    <div>
+      <section className="supportedBy fixed">
+        <h1>協賛募集中</h1>
+      </section>
+    </div>
+  );
+}
